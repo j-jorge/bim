31d896d4cb6e9d2937429287daceac324b0c9b96
Julien Jorge <julien.jorge@stuff-o-matic.com>
2023-06-03 18:05:39 +0200

    [game] Add the game archive, to serialize the game state.

---
+++ modules/game/main/include/bm/game/output_archive.impl.hpp
@@ -0,0 +1,34 @@
+/*
+  Copyright (C) 2023 Julien Jorge
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+template <typename T>
+void bm::game::output_archive::operator()(const T& value) const
+{
+  const char* data = reinterpret_cast<const char*>(std::addressof(value));
+  m_storage.insert(m_storage.end(), data, data + sizeof(T));
+}
+
+template <typename T>
+void bm::game::output_archive::operator()(entt::entity entity,
+                                          const T& value) const
+{
+  m_storage.reserve(m_storage.size() + sizeof(entity) + sizeof(T));
+
+  operator()(entity);
+  operator()(value);
+}
