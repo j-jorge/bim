928d6c57eba4dcccc056bc9a27e00f5f8586fe75
Julien Jorge <julien.jorge@stuff-o-matic.com>
2024-02-28 13:34:26 +0100

    Add a GitHub workflow to test the builds.

---
+++ ci/initialize-android-keystore.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -euo pipefail
+
+usage()
+{
+    cat <<EOF
+Fill the keystore.properties file for the Android build, and the
+keystore file itself for release builds. The values are taken from
+environment variables BIM_KEYSTORE_PASSWORD and BIM_RELEASE_KEYSTORE.
+
+Usage: $0 OPTIONS
+
+Where OPTIONS are:
+  --build-type TYPE
+      Tell if we are doing a "release" build or a "debug" build. For
+      the latter we will use the default debug signing key.
+  --help, -h
+      Display this message and exit.
+  --output-dir DIR
+      Where to put the generated files.
+EOF
+}
+
+while (( $# != 0 ))
+do
+    arg="$1"
+    shift
+
+    case "$arg" in
+        --build-type)
+            if [[ "$1" = debug ]] || [[ "$1" = release ]]
+            then
+                build_type="$1"
+            else
+                echo "Unknown build type '$1'." >&2
+                exit 1
+            fi
+            shift
+            ;;
+        --help)
+            usage
+            exit 0
+            ;;
+        --output-dir)
+            if (( "$#" == 0 ))
+            then
+                echo "Missing value for --output-dir." >&2
+                exit 1
+            fi
+            output_dir="$1"
+            shift
+            ;;
+        *)
+            echo "Unknown argument '$arg'." >&2
+            ;;
+    esac
+done
+
+if [[ -z "${build_type:-}" ]]
+then
+    echo "Option --build-type is required. See --help for details." >&2
+    exit 1
+fi
+
+if [[ -z "${output_dir:-}" ]]
+then
+    echo "Option --output-dir is required. See --help for details." >&2
+    exit 1
+fi
+
+if [[ ! -d "$output_dir" ]]
+then
+    echo "The directory '$output_dir' does not exist." >&2
+    exit 1
+fi
+
+if [[ "$build_type" = release ]]
+then
+    echo "$BIM_RELEASE_KEYSTORE" \
+        | base64 --decode > "$output_dir"/release.keystore
+    cat > "$output_dir"/keystore.properties <<EOF
+storePassword=$BIM_KEYSTORE_PASSWORD
+keyPassword=$BIM_KEYSTORE_PASSWORD
+EOF
+else
+    cat > "$output_dir"/keystore.properties <<EOF
+storePassword=
+keyPassword=
+EOF
+fi
---
+++ ci/install-minimal-environment.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -euo pipefail
+
+usage()
+{
+    cat <<EOF
+Install the build dependencies, for those not handled by the internal
+package manager.
+
+Usage: $0 OPTIONS
+
+Where OPTIONS are:
+  --compiler COMPILER
+      Install this C++ compiler (either g++ or clang-N).
+  --help, -h
+      Display this message and exit.
+  --target-platform PLATFORM
+      Configure to build for this platform (either android or linux).
+EOF
+}
+
+while (( $# != 0 ))
+do
+    arg="$1"
+    shift
+
+    case "$arg" in
+        --compiler)
+            if [[ "$1" = g++ ]] || [[ "$1" == clang-* ]]
+            then
+                compiler="$1"
+            else
+                echo "Unknown compiler '$1'." >&2
+                exit 1
+            fi
+            shift
+            ;;
+        --help)
+            usage
+            exit 0
+            ;;
+        --target-platform)
+            if [[ "$1" != linux ]] && [[ "$1" != android ]]
+            then
+                echo "Unknown target platform '$1'." >&2
+                exit 1
+            fi
+            target_platform="$1"
+            shift
+            ;;
+        *)
+            echo "Unknown argument '$arg'." >&2
+            ;;
+    esac
+done
+
+[[ -n "${compiler:-}" ]] \
+    || (echo "Option --compiler is required." >&2; exit 1)
+[[ -n "${target_platform:-}" ]] \
+    || (echo "Option --target-platform is required." >&2; exit 1)
+
+packages=(bzip2
+          ccache
+          cmake
+          curl
+          file
+          git
+          ninja-build
+          python3-venv
+          "$compiler")
+
+case "$target_platform" in
+    linux)
+        packages+=(libgtk-3-dev)
+        ;;
+    android)
+        packages+=(gradle
+                   libjpeg-turbo8-dev
+                   libpng-dev)
+        ;;
+esac
+
+apt-get update
+apt-get install --yes "${packages[@]}"
+
+if [[ "$compiler" == clang-* ]]
+then
+    clang_version="${compiler/*-/}"
+
+    update-alternatives \
+        --install /usr/bin/clang clang \
+        /usr/bin/clang-"$clang_version" 60
+    update-alternatives \
+        --install /usr/bin/cc cc /usr/bin/clang-"$clang_version" 60
+    update-alternatives --set cc /usr/bin/clang-"$clang_version"
+
+    update-alternatives \
+        --install /usr/bin/clang++ clang++ \
+        /usr/bin/clang++-"$clang_version" 60
+    update-alternatives \
+        --install /usr/bin/c++ c++ /usr/bin/clang++-"$clang_version" 60
+    update-alternatives --set c++ /usr/bin/clang++-"$clang_version"
+fi
+
+
