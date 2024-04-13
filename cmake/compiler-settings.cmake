0b8b13d4c63be9a3e94edf4e75895804252e91e4
Julien Jorge <julien.jorge@stuff-o-matic.com>
2024-04-13 22:39:00 +0200

    Compile with -Werror.

--- cmake/compiler-settings.cmake
+++ cmake/compiler-settings.cmake
@@ -2,7 +2,7 @@ set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL GNU)
-  add_compile_options(-Wall -pedantic)
+  add_compile_options(-Wall -pedantic -Werror)
 
   option(BIM_ADDRESS_SANITIZER "Compile with AddressSanitizer enabled" OFF)
 
