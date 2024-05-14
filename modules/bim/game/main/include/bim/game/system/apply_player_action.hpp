31a1ce0993eab03cbc61f6d8daeb8af2c5d8ac6e
Julien Jorge <julien.jorge@stuff-o-matic.com>
2024-05-14 23:24:32 +0200

    Reduce the speed of the players.

--- modules/bim/game/main/include/bim/game/system/apply_player_action.hpp
+++ modules/bim/game/main/include/bim/game/system/apply_player_action.hpp
@@ -22,7 +22,7 @@ namespace bim::game
 {
   class arena;
 
-  constexpr int g_player_steps_per_cell = 8;
+  constexpr int g_player_steps_per_cell = 16;
 
   void apply_player_action(entt::registry& registry, arena& arena);
 }
