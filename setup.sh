d39f021da12245c1e26ac4c224286a20adc7c23d
Julien Jorge <julien.jorge@stuff-o-matic.com>
2024-06-15 22:14:10 +0200

    [build] Set symbol visibility to hidden.

--- setup.sh
+++ setup.sh
@@ -17,6 +17,8 @@ all_build_steps=(dependencies configure build test)
 
 export bim_host_prefix="$host_prefix"
 export PATH="$script_dir/setup/bin/:$PATH"
+export CFLAGS="-fvisibility=hidden"
+export CXXFLAGS="-fvisibility=hidden"
 
 check_host_dependency()
 {
