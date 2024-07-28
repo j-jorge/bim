39af629cde63297699a38445265030e8d8b4034a
Julien Jorge <julien.jorge@stuff-o-matic.com>
2024-07-28 22:45:04 +0200

    Add bim::axmol::widget::apply_actions.

---
+++ modules/axmol/widget/main/include/bim/axmol/widget/apply_actions.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <bim/axmol/widget/named_node_group.hpp>
+
+namespace iscool::style
+{
+  class declaration;
+}
+
+namespace bim::axmol::action
+{
+  class runner;
+}
+
+namespace bim::axmol::widget
+{
+  class context;
+
+  void apply_actions(bim::axmol::action::runner& runner,
+                     const context& context, const named_node_group& nodes,
+                     const iscool::style::declaration& style);
+}
---
+++ modules/axmol/widget/main/src/bim/axmol/widget/apply_actions.cpp
@@ -0,0 +1,56 @@
+#include <bim/axmol/widget/apply_actions.hpp>
+
+#include <bim/axmol/widget/context.hpp>
+#include <bim/axmol/widget/log_context.hpp>
+
+#include <bim/axmol/action/dynamic_factory.hpp>
+#include <bim/axmol/action/runner.hpp>
+
+#include <bim/unreachable.hpp>
+
+#include <iscool/log/log.hpp>
+#include <iscool/log/nature/error.hpp>
+#include <iscool/style/declaration.hpp>
+
+#include <axmol/2d/ActionInterval.h>
+
+void bim::axmol::widget::apply_actions(bim::axmol::action::runner& runner,
+                                       const context& context,
+                                       const named_node_group& nodes,
+                                       const iscool::style::declaration& style)
+{
+  const named_node_group::const_iterator nodes_end = nodes.end();
+
+  for (const auto& e : style.get_declarations())
+    {
+      const std::string& node_name = e.first;
+      const iscool::style::declaration& substyle = e.second;
+
+      const bim::axmol::widget::named_node_group::const_iterator node =
+          nodes.find(node_name);
+
+      if (node == nodes_end)
+        {
+          bim_unreachable_in_release;
+          ic_log(iscool::log::nature::error(),
+                 bim::axmol::widget::g_log_context, "No node named '%s'.",
+                 node_name);
+          continue;
+        }
+
+      const bim::axmol::ref_ptr<ax::FiniteTimeAction> action =
+          context.action_factory.create(context.colors, substyle);
+
+      if (!action)
+        {
+          bim_unreachable_in_release;
+          ic_log(iscool::log::nature::error(),
+                 bim::axmol::widget::g_log_context, "No action for node '%s'.",
+                 node_name);
+          continue;
+        }
+
+      runner.run(
+          *ax::TargetedAction::create(node->second.get(), action.get()));
+    }
+}
