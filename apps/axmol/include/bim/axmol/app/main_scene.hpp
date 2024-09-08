ca3a27bd10c7541f034ae1e987a3cccfef463354
Julien Jorge <julien.jorge@stuff-o-matic.com>
2024-09-08 17:25:00 +0200

    API to display overlays.

--- apps/axmol/include/bim/axmol/app/main_scene.hpp
+++ apps/axmol/include/bim/axmol/app/main_scene.hpp
@@ -4,6 +4,8 @@
 #include <bim/axmol/input/tree.hpp>
 #include <bim/axmol/widget/declare_controls_struct.hpp>
 
+#include <unordered_map>
+
 namespace ax
 {
   class Scene;
@@ -32,6 +34,13 @@ namespace bim::axmol::app
 
     void add_in_main_canvas(ax::Node& node,
                             const bim::axmol::input::node_reference& inputs);
+    void add_in_overlays(ax::Node& node,
+                         const bim::axmol::input::node_reference& inputs);
+    void remove_from_overlays(ax::Node& node);
+
+  private:
+    using node_to_inputs_map =
+        std::unordered_map<const ax::Node*, bim::axmol::input::node_reference>;
 
   private:
     ax::Scene& m_scene;
@@ -39,6 +48,9 @@ namespace bim::axmol::app
     bim_declare_controls_struct(controls, m_controls, 2);
 
     bim::axmol::input::tree m_inputs;
-    bim::axmol::input::tree m_main_area_inputs;
+    bim::axmol::input::tree m_main_canvas_inputs;
+    bim::axmol::input::tree m_overlay_inputs;
+
+    node_to_inputs_map m_overlay_node_inputs;
   };
 }
--- apps/axmol/src/bim/axmol/app/main_scene.cpp
+++ apps/axmol/src/bim/axmol/app/main_scene.cpp
@@ -4,6 +4,9 @@
 #include <bim/axmol/widget/add_group_as_children.hpp>
 #include <bim/axmol/widget/apply_bounds.hpp>
 #include <bim/axmol/widget/context.hpp>
+#include <bim/axmol/widget/factory/node.hpp>
+
+#include <bim/assume.hpp>
 
 #define x_widget_scope bim::axmol::app::main_scene::
 #define x_widget_type_name controls
@@ -18,7 +21,8 @@ bim::axmol::app::main_scene::main_scene(
   : m_scene(scene)
   , m_controls(context, *style.get_declaration("widgets"))
 {
-  m_inputs.push_back(m_main_area_inputs.root());
+  m_inputs.push_back(m_overlay_inputs.root());
+  m_inputs.push_back(m_main_canvas_inputs.root());
 
   bim::axmol::widget::add_group_as_children(scene, m_controls->all_nodes);
   bim::axmol::widget::apply_bounds(context.style_cache, m_controls->all_nodes,
@@ -47,7 +51,25 @@ void bim::axmol::app::main_scene::add_in_main_canvas(
   // Align the node's bottom-left corner with its parent's.
   node.setPosition(node.getAnchorPoint() * size);
 
-  // TODO: map the inputs with the node, split m_inputs to have one tree for
-  // each layer.
-  m_inputs.push_back(inputs);
+  m_main_canvas_inputs.push_back(inputs);
+}
+
+void bim::axmol::app::main_scene::add_in_overlays(
+    ax::Node& node, const bim::axmol::input::node_reference& inputs)
+{
+  m_controls->overlay->addChild(&node);
+
+  m_overlay_inputs.push_back(inputs);
+  m_overlay_node_inputs.insert(node_to_inputs_map::value_type(&node, inputs));
+}
+
+void bim::axmol::app::main_scene::remove_from_overlays(ax::Node& node)
+{
+  const node_to_inputs_map::iterator it = m_overlay_node_inputs.find(&node);
+  bim_assume(it != m_overlay_node_inputs.end());
+
+  m_overlay_inputs.erase(it->second);
+  m_overlay_node_inputs.erase(it);
+
+  m_controls->overlay->removeChild(&node);
 }
