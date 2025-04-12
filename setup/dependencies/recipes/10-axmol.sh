f88cdda4c19c6b8372947873e17258bf8a2978f3
Julien Jorge <julien.jorge@stuff-o-matic.com>
2025-04-12 15:13:33 +0200

    Update Axmol to 2.5.0.

--- setup/dependencies/recipes/10-axmol.sh
+++ setup/dependencies/recipes/10-axmol.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 : "${bim_packages_root:-}"
 
 : "${axmol_repository:=https://github.com/j-jorge/axmol/}"
-: "${axmol_version:=2.4.0j}"
+: "${axmol_version:=2.5.0j}"
 package_revision=1
 version="$axmol_version"-"$package_revision"
 
