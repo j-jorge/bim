77674ce2afec536e50aface8caab226285de8609
Julien Jorge <julien.jorge@stuff-o-matic.com>
2025-04-21 21:41:41 +0200

    clangtidy

--- modules/bim/game/main/include/bim/game/component/player_action_queue.hpp
+++ modules/bim/game/main/include/bim/game/component/player_action_queue.hpp
@@ -3,6 +3,9 @@
 
 #include <bim/game/component/player_action.hpp>
 
+#include <cstddef>
+#include <cstdint>
+
 namespace bim::game
 {
   struct queued_action
@@ -10,13 +13,14 @@ namespace bim::game
     player_action action;
 
     /// Where the player was when the action was scheduled.
-    uint8_t arena_x;
-    uint8_t arena_y;
+    std::uint8_t arena_x;
+    std::uint8_t arena_y;
   };
 
   struct player_action_queue
   {
-    queued_action enqueue(player_action a, uint8_t arena_x, uint8_t arena_y);
+    queued_action enqueue(player_action a, std::uint8_t arena_x,
+                          std::uint8_t arena_y);
 
     static constexpr std::size_t queue_size = 8;
     queued_action m_queue[queue_size];
