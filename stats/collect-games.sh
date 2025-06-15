947cda919c711a954c4401c10eaa24233affd405
Julien Jorge <julien.jorge@stuff-o-matic.com>
2025-06-15 21:10:09 +0200

    Count created games in the collect-games stats script.

--- stats/collect-games.sh
+++ stats/collect-games.sh
@@ -2,9 +2,7 @@
 
 set -euo pipefail
 
-grep -h 'Sending game over' "$@" \
-    | sed 's/^.\([0-9]\+-[0-9]\+-[0-9]\+\) \([0-9]\+\)\+.\+game_id=\([0-9]\+\).\+/\1-\2 \3/' \
-    | uniq \
-    | cut -d' ' -f1 \
+grep -h 'Creating new game' "$@" \
+    | sed 's/^.\([0-9]\+-[0-9]\+-[0-9]\+\) \([0-9]\+\)\+.\+/\1-\2/' \
     | uniq -c \
     | awk '{print $2, $1}'
