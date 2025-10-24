df9a307edbe998f7f3310b4b5c289e0ca36ad069
Julien Jorge <julien.jorge@stuff-o-matic.com>
2025-10-24 19:39:43 +0200

    Action sequence creation from a style.

---
+++ modules/axmol/action/main/include/bim/axmol/action/factory/sequence.hpp
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+#pragma once
+
+#include <bim/axmol/ref_ptr.hpp>
+
+namespace iscool::style
+{
+  class declaration;
+}
+
+namespace ax
+{
+  class ActionInterval;
+}
+
+namespace bim::axmol
+{
+  class colour_chart;
+
+  namespace action
+  {
+    class dynamic_factory;
+
+    [[nodiscard]] bim::axmol::ref_ptr<ax::ActionInterval>
+    sequence_from_style(const dynamic_factory& factory,
+                        const bim::axmol::colour_chart& colors,
+                        const iscool::style::declaration& style);
+  }
+}
