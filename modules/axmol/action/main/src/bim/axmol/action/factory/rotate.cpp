a225da06c8aa7b6e63ac5ec129e9a9a71aae33a1
Julien Jorge <julien.jorge@stuff-o-matic.com>
2025-10-24 19:25:23 +0200

    Handle both "to" and "by" properties for rotations from style.

--- modules/axmol/action/main/src/bim/axmol/action/factory/rotate.cpp
+++ modules/axmol/action/main/src/bim/axmol/action/factory/rotate.cpp
@@ -12,8 +12,13 @@
 bim::axmol::ref_ptr<ax::ActionInterval>
 bim::axmol::action::rotate_from_style(const iscool::style::declaration& style)
 {
-  ax::ActionInterval* rotate = ax::RotateBy::create(
-      *style.get_number("duration"), *style.get_number("by"));
+  const float duration = *style.get_number("duration");
+  const iscool::optional<float> to = style.get_number("to");
+  const iscool::optional<float> by = style.get_number("by");
+
+  ax::ActionInterval* rotate =
+      by ? (ax::ActionInterval*)ax::RotateBy::create(duration, *by)
+         : (ax::ActionInterval*)ax::RotateTo::create(duration, *to);
   rotate = maybe_wrap_in_easing_function(*rotate, style);
 
   const iscool::optional<float> from = style.get_number("from");
