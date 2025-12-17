708eb58f78634d65921bf34349ec6863bd0c3c1c
Julien Jorge <julien.jorge@stuff-o-matic.com>
2025-12-17 20:43:00 +0100

    Add missing license identifier in source files.

--- modules/axmol/input/main/src/bim/axmol/input/observer/scroll_view_glue.cpp
+++ modules/axmol/input/main/src/bim/axmol/input/observer/scroll_view_glue.cpp
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-only
 #include <bim/axmol/input/observer/scroll_view_glue.hpp>
 
 #include <bim/axmol/input/touch_event.hpp>
--- modules/bim/app/java/main/bim/app/IShop.java
+++ modules/bim/app/java/main/bim/app/IShop.java
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-only
 package bim.app;
 
 import java.util.List;
