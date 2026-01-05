64a0e77b7aef884626c0d5bc7c190fe9a50f6f50
Julien Jorge <julien.jorge@stuff-o-matic.com>
2026-01-05 20:55:35 +0100

    Add some tests for the shield power-ups.

--- modules/bim/game/tests/src/bim/game/system/update_shield_power_ups.cpp
+++ modules/bim/game/tests/src/bim/game/system/update_shield_power_ups.cpp
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-#include <bim/game/system/update_invisibility_power_ups.hpp>
+#include <bim/game/system/update_shield_power_ups.hpp>
 
 #include <bim/game/entity_world_map.hpp>
 
 #include <bim/game/component/dead.hpp>
-#include <bim/game/component/invisibility_state.hpp>
+#include <bim/game/component/player.hpp>
+#include <bim/game/component/shield.hpp>
 #include <bim/game/factory/player.hpp>
 #include <bim/game/factory/power_up.hpp>
 
@@ -14,10 +15,10 @@
 
 namespace bim::game
 {
-  class invisibility_power_up;
+  class shield_power_up;
 }
 
-TEST(update_invisibility_power_ups, player_collision)
+TEST(update_shield_power_ups, player_collision)
 {
   entt::registry registry;
   bim::game::entity_world_map entity_map(3, 3);
@@ -25,28 +26,26 @@ TEST(update_invisibility_power_ups, player_collision)
   const int y = 1;
 
   const entt::entity power_up_entity =
-      power_up_factory<bim::game::invisibility_power_up>(registry, entity_map,
-                                                         x, y);
+      power_up_factory<bim::game::shield_power_up>(registry, entity_map, x, y);
   const entt::entity player_entity = bim::game::player_factory(
       registry, entity_map, 0, x, y, bim::game::animation_id{});
 
-  bim::game::update_invisibility_power_ups(registry, entity_map);
+  bim::game::update_shield_power_ups(registry, entity_map);
 
-  EXPECT_TRUE(bim::game::is_invisible(registry, player_entity));
+  EXPECT_TRUE(registry.storage<bim::game::shield>().contains(player_entity));
   EXPECT_TRUE(registry.storage<bim::game::dead>().contains(power_up_entity));
   ASSERT_EQ(1, entity_map.entities_at(x, y).size());
   EXPECT_EQ(player_entity, entity_map.entities_at(x, y)[0]);
 }
 
-TEST(update_invisibility_power_ups, two_players_only_one_gets_the_power_up)
+TEST(update_shield_power_ups, two_players_only_one_gets_the_power_up)
 {
   entt::registry registry;
   bim::game::entity_world_map entity_map(3, 3);
   const int x = 1;
   const int y = 1;
 
-  power_up_factory<bim::game::invisibility_power_up>(registry, entity_map, x,
-                                                     y);
+  power_up_factory<bim::game::shield_power_up>(registry, entity_map, x, y);
 
   const entt::entity player_entity[2] = {
     bim::game::player_factory(registry, entity_map, 0, x, y,
@@ -55,11 +54,13 @@ TEST(update_invisibility_power_ups, two_players_only_one_gets_the_power_up)
                               bim::game::animation_id{})
   };
 
-  EXPECT_FALSE(bim::game::is_invisible(registry, player_entity[0]));
-  EXPECT_FALSE(bim::game::is_invisible(registry, player_entity[1]));
+  EXPECT_FALSE(
+      registry.storage<bim::game::shield>().contains(player_entity[0]));
+  EXPECT_FALSE(
+      registry.storage<bim::game::shield>().contains(player_entity[1]));
 
-  bim::game::update_invisibility_power_ups(registry, entity_map);
+  bim::game::update_shield_power_ups(registry, entity_map);
 
-  EXPECT_NE(bim::game::is_invisible(registry, player_entity[0]),
-            bim::game::is_invisible(registry, player_entity[1]));
+  EXPECT_NE(registry.storage<bim::game::shield>().contains(player_entity[0]),
+            registry.storage<bim::game::shield>().contains(player_entity[1]));
 }
