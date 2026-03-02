0ac3efc34b5fe93453841cd15de618f693310c0b
Julien Jorge <julien.jorge@stuff-o-matic.com>
2026-03-02 21:50:36 +0100

    Compile for x86-64-v3 architectures by default.

--- cmake/compiler-settings.cmake
+++ cmake/compiler-settings.cmake
@@ -12,6 +12,16 @@ if ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     -fno-omit-frame-pointer
   )
 
+  option(BIM_GENERIC_HARDWARE
+    "Compile for a generic hardware architecture (compilers's default)."
+    OFF
+  )
+  message("Targeting Generic hardware: ${BIM_GENERIC_HARDWARE}")
+
+  if (NOT BIM_BUILDING_FOR_ANDROID AND NOT BIM_GENERIC_HARDWARE)
+    add_compile_options(-march=x86-64-v3)
+  endif()
+
   # F-Droid requires identical binaries between its build and the
   # reference APK, thus we need a reproducible build for Android
   # release builds. We are going to drop the build ID in this case as
